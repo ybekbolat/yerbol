8c6ead0a3cd780791ef2737907120cbff4c4b956
ybekbolat <57897774+ybekbolat@users.noreply.github.com>
2019-11-23 13:41:30 +0600

    Add files via upload

---
+++ 144.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    int counter = 0;
+    for(a=3;a<=100;a++)
+    {
+        for(b=a+1;b<=100;b++)
+        {
+            c=sqrt(a*a+b*b);
+            if (c<=100 && a*a+b*b==c*c)
+            {
+                counter++;
+            }
+        }
+    }
+    cout<<counter;
+    return 0;
+}
